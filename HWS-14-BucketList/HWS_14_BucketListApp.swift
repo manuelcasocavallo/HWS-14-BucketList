d7003619572cc6a0c372d41d321061fc8a8bf3d2
Manuel Casocavallo <manuelcasocavallo@gmail.com>
2021-05-06 15:45:43 +0200

    Initial Commit

---
+++ HWS-14-BucketList/HWS_14_BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_14_BucketListApp.swift
+//  HWS-14-BucketList
+//
+//  Created by Manuel Casocavallo on 06/05/21.
+//
+
+import SwiftUI
+
+@main
+struct HWS_14_BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
